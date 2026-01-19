3de12480f07c576eb2476638c9ffeafe2963eec0
Hunter <h_kruse@u.pacific.edu>
2026-01-18 22:18:56 -0800

    Added quest 1

---
+++ Week1Assignment/lib/Scene/BackgroundRays.js
@@ -0,0 +1,29 @@
+import Standard2DVertexObject from "/lib/Scene/Standard2DVertexObject.js"
+
+ export default class BackgroundRays extends Standard2DVertexObject {
+   constructor(device, canvasFormat) {
+     let vertices = new Float32Array([
+     // x, y
+        0,.4, 
+        -1,.4, 
+        -1,-.6,
+
+        0, .4,
+        1, .4,
+        1, 1,
+
+        0,.4,
+        0,1,
+        -.6,1,
+
+        0,.4,
+        0,-1,
+        1,-1
+
+        
+
+      ]);
+     super(device, canvasFormat, vertices, '/lib/Shaders/optimized_BackgroundRays.wgsl', 'triangle-strip');
+     this._vertices = vertices;
+   }
+ }
